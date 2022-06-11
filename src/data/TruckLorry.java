78330f2958de72e30ef7a4e0c0c58c2ec86a8e96
kumardk0001 <kumardhiraj0001@gmail.com>
2022-06-11 15:59:41 +0530

    TruckLorry implementation added

--- src/data/TruckLorry.java
+++ src/data/TruckLorry.java
@@ -1,4 +1,34 @@
 package data;
 
-public class TruckLorry {
+import java.util.Scanner;
+
+public class TruckLorry extends Vehicle {
+
+    private static TruckLorry truckLorry;
+
+
+    public static TruckLorry getBike() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome To Bike Type Vehicle");
+        if (truckLorry != null) {
+            System.out.println("Enter bike color : ");
+            String color = sc.next();
+            System.out.println("Enter bike brand : ");
+            String brand = sc.next();
+            System.out.println("Enter bike type : ");
+            String type = sc.next();
+            System.out.println("Enter bike regId : ");
+            String regId = sc.next();
+
+            truckLorry.setColor(color);
+            truckLorry.setBrand(brand);
+            truckLorry.setRegId(regId);
+            truckLorry.setType(type);
+        } else {
+            truckLorry = new TruckLorry();
+        }
+
+        sc.close();
+        return truckLorry;
+    }
 }
